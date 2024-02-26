9d97f8f2360a7bfcc7ae1ffc7079422173679dfc
Matheus Alves <contato.mathsalvs@gmail.com>
2024-02-26 17:01:00 -0300

    Update Sveltekit and Vite configs

--- svelte.config.js
+++ svelte.config.js
@@ -2,21 +2,24 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
-		alias: {
-			'@lib': 'src/lib',
-			'@components': 'src/lib/components',
-			'@stores': 'src/lib/stores'
-		}
-	},
-	preprocess: vitePreprocess()
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true,
+        }),
+        alias: {
+            '@lib': 'src/lib',
+            '@content': 'src/lib/content',
+            '@components': 'src/lib/components',
+            '@sections': 'src/lib/sections',
+            '@stores': 'src/lib/stores',
+            '@utils': 'src/lib/utils',
+        },
+    },
+    preprocess: vitePreprocess(),
 };
 
 export default config;
