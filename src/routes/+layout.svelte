7e4c088629be46653b854b8bd25d74f69e25e9c2
Matheus Alves <contato.mathsalvs@gmail.com>
2024-02-27 17:01:00 -0300

    Update layout files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,9 @@
 <script>
-	import { locale, locales } from '$stores/language';
+    import AnalyticsModal from '@components/AnalyticsModal.svelte';
 
-	import AnalyticsModal from '$components/AnalyticsModal.svelte';
-
-	import '../app.css';
-	import '../text.css';
+    import '../app.css';
+    import '../text.css';
 </script>
 
 <slot />
 <AnalyticsModal />
-<select bind:value={$locale}>
-	{#each locales as l}
-		<option value={l}>{l}</option>
-	{/each}
-</select>
-
-<style>
-</style>
