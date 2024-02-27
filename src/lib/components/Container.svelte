1bde383267a2ec882581e30c4940e907b6da1ea7
Matheus Alves <contato.mathsalvs@gmail.com>
2024-02-27 17:02:00 -0300

    Update Container and Footer components

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,54 +1,54 @@
 <script>
 </script>
 
-<div class="odd-content-container">
-	<slot />
+<div>
+    <slot />
 </div>
 
 <style>
-	div {
-		width: 100%;
-	}
+    div {
+        width: 100%;
+    }
 
-	@media only screen and (max-width: 640px) {
-		div {
-			margin: 0 auto;
-			width: 85vw;
-		}
-	}
+    @media only screen and (max-width: 600px) {
+        div {
+            margin: 0 auto;
+            width: 85vw;
+        }
+    }
 
-	@media only screen and (min-width: 640px) {
-		div {
-			margin: 0 auto;
-			width: 80vw;
-		}
-	}
+    @media only screen and (min-width: 600px) {
+        div {
+            margin: 0 auto;
+            width: 80vw;
+        }
+    }
 
-	@media only screen and (min-width: 768px) {
-		div {
-			margin: 0 auto;
-			width: 75vw;
-		}
-	}
+    @media only screen and (min-width: 768px) {
+        div {
+            margin: 0 auto;
+            width: 75vw;
+        }
+    }
 
-	@media only screen and (min-width: 1024px) {
-		div {
-			margin: 0 auto;
-			width: 70vw;
-		}
-	}
+    @media only screen and (min-width: 1024px) {
+        div {
+            margin: 0 auto;
+            width: 70vw;
+        }
+    }
 
-	@media only screen and (min-width: 1280px) {
-		div {
-			margin: 0 auto;
-			width: 65vw;
-		}
-	}
+    @media only screen and (min-width: 1280px) {
+        div {
+            margin: 0 auto;
+            width: 65vw;
+        }
+    }
 
-	@media only screen and (min-width: 1536px) {
-		div {
-			margin: 0 auto;
-			width: 75rem;
-		}
-	}
+    @media only screen and (min-width: 1536px) {
+        div {
+            margin: 0 auto;
+            width: 75rem;
+        }
+    }
 </style>
---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,92 @@
+<script>
+    export let pattern = null;
+</script>
+
+<footer class={pattern ? `odd-${pattern}` : ''}>
+    <a href="/">
+        <img src="/brand/full-logo.svg" alt="Odd Data & Design Studio" />
+    </a>
+    <div>
+        <a
+            href="https://www.linkedin.com/company/oddstudio/"
+            rel="noopener noreferrer"
+        >
+            <img src="/brand/linkedin.svg" alt="Linkedin" />
+        </a>
+        <a
+            href="https://www.instagram.com/weareoddstudio/"
+            rel="noopener noreferrer"
+        >
+            <img src="/brand/instagram.svg" alt="Instagram" />
+        </a>
+    </div>
+</footer>
+
+<style>
+    footer {
+        align-items: flex-end;
+        background: var(--odd-purple-dark);
+        display: flex;
+        justify-content: space-between;
+    }
+
+    footer.odd-studio {
+        background: var(--odd-purple-dark);
+    }
+
+    footer.odd-experiments {
+        background: var(--odd-turquoise-medium);
+    }
+
+    footer.odd-education {
+        background: var(--odd-blue-medium);
+    }
+
+    a,
+    img {
+        cursor: pointer;
+    }
+
+    div {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    @media only screen and (max-width: 600px) {
+        footer {
+            padding: 1.5rem 7.5vw 2rem 7.5vw;
+        }
+
+        footer > a,
+        footer > a img {
+            height: 3rem;
+            width: calc(3rem * 2.35);
+        }
+
+        footer > div a,
+        footer > div img {
+            height: 1.5rem;
+            width: 1.5rem;
+        }
+
+        div {
+            gap: 1rem;
+        }
+    }
+
+    @media only screen and (min-width: 600px) {
+    }
+
+    @media only screen and (min-width: 768px) {
+    }
+
+    @media only screen and (min-width: 1024px) {
+    }
+
+    @media only screen and (min-width: 1280px) {
+    }
+
+    @media only screen and (min-width: 1536px) {
+    }
+</style>
