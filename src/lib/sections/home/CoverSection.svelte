01761723c7852f83a1fca33c297d58893308ff78
Matheus Alves <contato.mathsalvs@gmail.com>
2024-02-29 17:02:00 -0300

    Add Cover and Description sections (home)

---
+++ src/lib/sections/home/CoverSection.svelte
@@ -0,0 +1,73 @@
+<script>
+    import Container from '@components/Container.svelte';
+    import ScrollSymbol from '@components/ScrollSymbol.svelte';
+</script>
+
+<section>
+    <Container>
+        <h1>we reimage <span class="highlighted-text">data</span> into</h1>
+        <ScrollSymbol />
+    </Container>
+</section>
+
+<style>
+    section {
+        background: linear-gradient(
+            180deg,
+            var(--odd-purple-dark) 0%,
+            var(--odd-purple-medium) 75%,
+            var(--odd-gray-light) 100%
+        );
+    }
+
+    h1 {
+        color: var(--odd-gray-light);
+        font-family: 'Newsreader', serif;
+        font-weight: 300;
+        letter-spacing: -0.2rem;
+        line-height: 95%;
+        text-align: center;
+    }
+
+    h1 .highlighted-text {
+        font-style: italic;
+        font-weight: 400;
+    }
+
+    @media only screen and (max-width: 600px) {
+        section {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+            min-height: 100svh;
+            padding: 2.5rem 0;
+        }
+
+        section :global(div) {
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+
+        h1 {
+            font-size: 4rem;
+            margin-bottom: 50svh;
+        }
+    }
+
+    @media only screen and (min-width: 600px) {
+    }
+
+    @media only screen and (min-width: 768px) {
+    }
+
+    @media only screen and (min-width: 1024px) {
+    }
+
+    @media only screen and (min-width: 1280px) {
+    }
+
+    @media only screen and (min-width: 1536px) {
+    }
+</style>
---
+++ src/lib/sections/home/DescriptionSection.svelte
@@ -0,0 +1,103 @@
+<script>
+    import { inView } from '@utils/actions';
+
+    import Container from '@components/Container.svelte';
+    import SectionTitle from '@components/SectionTitle.svelte';
+    import HeaderText from '@components/HeaderText.svelte';
+
+    let showTexts = false;
+</script>
+
+<section>
+    <Container>
+        <SectionTitle pattern="studio">What we do</SectionTitle>
+        <HeaderText background="light" pattern="studio">
+            <p>
+                Odd Data & Design is a <span
+                    class="highlight-font highlight-color"
+                    >data reimagination</span
+                >
+                company specializing in
+                <span class="highlight-font highlight-color"
+                    >data representation</span
+                >.
+            </p>
+        </HeaderText>
+        <div
+            class={`description-wrapper ${showTexts ? 'reveal' : ''}`}
+            use:inView
+            on:enter={() => (showTexts = true)}
+        >
+            <p>
+                We strategize, analyze, explore, visualize, and transform your
+            </p>
+            <p>
+                data, research, discoveries, information, ideas, and complex
+                concepts
+            </p>
+            <p>to drive action, decisions, inspiration, and conversations.</p>
+        </div>
+    </Container>
+</section>
+
+<style>
+    section {
+        background-color: var(--odd-gray-light);
+    }
+
+    .description-wrapper p {
+        background-position: center left;
+        background-repeat: no-repeat;
+        color: var(--odd-gray-dark);
+        display: block;
+        font-family: 'Switzer', sans-serif;
+        font-weight: 400;
+        letter-spacing: -0.03rem;
+        line-height: 120%;
+        text-wrap: balance;
+    }
+
+    .description-wrapper p:nth-child(1) {
+        background-image: url('/icons/description-one.svg');
+    }
+
+    .description-wrapper p:nth-child(2) {
+        background-image: url('/icons/description-two.svg');
+    }
+
+    .description-wrapper p:nth-child(3) {
+        background-image: url('/icons/description-three.svg');
+    }
+
+    @media only screen and (max-width: 600px) {
+        section {
+            padding: 5rem 0 2.5rem 0;
+        }
+
+        .description-wrapper {
+            margin-top: 5rem;
+        }
+
+        .description-wrapper p {
+            background-size: auto 3rem;
+            font-size: 1.5rem;
+            margin-bottom: 1rem;
+            padding-left: 1.75rem;
+        }
+    }
+
+    @media only screen and (min-width: 600px) {
+    }
+
+    @media only screen and (min-width: 768px) {
+    }
+
+    @media only screen and (min-width: 1024px) {
+    }
+
+    @media only screen and (min-width: 1280px) {
+    }
+
+    @media only screen and (min-width: 1536px) {
+    }
+</style>
