68bbdb9dabb989c0188211a3bcde1da329a57784
Matheus Alves <contato.mathsalvs@gmail.com>
2024-03-01 13:01:00 -0300

    Update language-related files

---
+++ src/lib/content/translations.js
@@ -0,0 +1,10 @@
+const translations = {
+    en: {
+        default: 'Hello, World!',
+    },
+    pt: {
+        default: 'Olá, Mundo!',
+    },
+};
+
+export default translations;
--- src/lib/stores/language.js
+++ src/lib/stores/language.js
@@ -1,27 +1,27 @@
 import { derived, writable } from 'svelte/store';
-import translations from '@lib/translations';
+import translations from '@content/translations';
 
 export const locale = writable('en');
 export const locales = Object.keys(translations);
 
 const translate = (locale, key, vars) => {
-	if (!key) throw new Error('No key provided to $t()!');
-	if (!locale) throw new Error(`No translation for key "${key}"!`);
+    if (!key) throw new Error('No key provided to $t()!');
+    if (!locale) throw new Error(`No translation for key "${key}"!`);
 
-	let text = translations[locale][key];
-	if (!text) throw new Error(`No translation found for ${locale}.${key}!`);
+    let text = translations[locale][key];
+    if (!text) throw new Error(`No translation found for ${locale}.${key}!`);
 
-	Object.keys(vars).map((k) => {
-		const regex = new RegExp(`{{${k}}}`, 'g');
-		text = text.replace(regex, vars[k]);
-	});
+    Object.keys(vars).map((k) => {
+        const regex = new RegExp(`{{${k}}}`, 'g');
+        text = text.replace(regex, vars[k]);
+    });
 
-	return text;
+    return text;
 };
 
 export const t = derived(
-	locale,
-	($locale) =>
-		(key, vars = {}) =>
-			translate($locale, key, vars)
+    locale,
+    ($locale) =>
+        (key, vars = {}) =>
+            translate($locale, key, vars)
 );
