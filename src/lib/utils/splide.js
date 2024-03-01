002f19e7230f2f61ff41b22264933ee949f3a320
Matheus Alves <contato.mathsalvs@gmail.com>
2024-03-01 13:00:00 -0300

    Add Splide configs and inView Svelte action

---
+++ src/lib/utils/splide.js
@@ -0,0 +1,12 @@
+export const quotesSliderConfig = {
+    arrows: false,
+    autoplay: true,
+    autoWidth: true,
+    interval: 5000,
+    padding: { left: '1rem', right: '1rem' },
+    resetProgress: false,
+    rewind: true,
+    rewindByDrag: true,
+    rewindSpeed: 750,
+    speed: 750,
+};
