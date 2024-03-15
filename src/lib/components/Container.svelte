a566baba30278da3e1774ac8707eb9b34ff34d8f
Matheus Alves <contato.mathsalvs@gmail.com>
2024-03-15 17:00:00 -0300

    Add minor adjustments for Studio's Case page

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,8 +1,7 @@
 <script>
-    export let padding = false;
 </script>
 
-<div class={padding ? 'with-padding' : ''}>
+<div>
     <slot />
 </div>
 
@@ -16,44 +15,40 @@
             margin: 0 auto;
             width: 85vw;
         }
-
-        div.with-padding {
-            padding: 3.75rem 0;
-        }
     }
 
     @media only screen and (min-width: 600px) {
-        div {
-            margin: 0 auto;
-            width: 80vw;
-        }
-    }
-
-    @media only screen and (min-width: 768px) {
         div {
             margin: 0 auto;
             width: 75vw;
         }
     }
 
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 768px) {
         div {
             margin: 0 auto;
             width: 70vw;
         }
     }
 
+    @media only screen and (min-width: 1024px) {
+        div {
+            margin: 0 auto;
+            width: 60vw;
+        }
+    }
+
     @media only screen and (min-width: 1280px) {
         div {
             margin: 0 auto;
-            width: 65vw;
+            width: 45rem;
         }
     }
 
     @media only screen and (min-width: 1536px) {
         div {
             margin: 0 auto;
-            width: 75rem;
+            width: 60rem;
         }
     }
 </style>
--- src/routes/studio/cases/[uid]/+page.svelte
+++ src/routes/studio/cases/[uid]/+page.svelte
@@ -3,36 +3,58 @@
     import { components } from '@slices';
 
     import Container from '@components/Container.svelte';
-    import CaseIntro from '@components/cases/CaseIntro.svelte';
-    import Subscription from '@components/Subscription.svelte';
+    import CaseIntro from '@components/CaseIntro.svelte';
     import Footer from '@components/Footer.svelte';
 
     export let data;
 </script>
 
-<Container padding={true}>
-    <CaseIntro content={data.page.data} />
-    <SliceZone slices={data.page.data.slices} {components} />
-</Container>
-<Subscription />
+<svelte:head>
+    <title>Odd Data & Design Studio</title>
+</svelte:head>
+
+<CaseIntro content={data.page.data} />
+<section>
+    <Container>
+        <SliceZone slices={data.page.data.slices} {components} />
+    </Container>
+</section>
 <Footer pattern="studio" />
 
 <style>
     @media only screen and (max-width: 600px) {
+        section {
+            margin-bottom: 3.75rem;
+        }
     }
 
     @media only screen and (min-width: 600px) {
+        section {
+            margin-bottom: 3.75rem;
+        }
     }
 
     @media only screen and (min-width: 768px) {
+        section {
+            margin-bottom: 3.75rem;
+        }
     }
 
     @media only screen and (min-width: 1024px) {
+        section {
+            margin-bottom: 3.75rem;
+        }
     }
 
     @media only screen and (min-width: 1280px) {
+        section {
+            margin-bottom: 5rem;
+        }
     }
 
     @media only screen and (min-width: 1536px) {
+        section {
+            margin-bottom: 5rem;
+        }
     }
 </style>
