a5186a10b4e820ec32c9fdeab0741a707e3df0e6
Matheus Alves <contato.mathsalvs@gmail.com>
2024-03-15 17:01:00 -0300

    Add desktop version for 'Subscription' and 'Footer' components

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,37 +1,44 @@
 <script>
+    import Subscription from '@components/Subscription.svelte';
+
     export let pattern = null;
 </script>
 
 <footer class={pattern ? `odd-${pattern}` : ''}>
-    <a href="/">
-        <img src="/brand/full-logo.svg" alt="Odd Data & Design Studio" />
-    </a>
-    <div>
-        <a
-            href="https://www.linkedin.com/company/oddstudio/"
-            rel="noopener noreferrer"
-        >
-            <img src="/brand/linkedin.svg" alt="Linkedin" />
+    <Subscription {pattern} />
+    <div id="footer-social-media">
+        <a href="/">
+            <img src="/brand/full-logo.svg" alt="Odd Data & Design Studio" />
         </a>
-        <a
-            href="https://www.instagram.com/weareoddstudio/"
-            rel="noopener noreferrer"
-        >
-            <img src="/brand/instagram.svg" alt="Instagram" />
-        </a>
+        <div>
+            <a
+                href="https://www.linkedin.com/company/oddstudio/"
+                rel="external noopener noreferrer"
+                target="_blank"
+            >
+                <img src="/brand/linkedin.svg" alt="Linkedin" />
+            </a>
+            <a
+                href="https://www.instagram.com/weareoddstudio/"
+                rel="external noopener noreferrer"
+                target="_blank"
+            >
+                <img src="/brand/instagram.svg" alt="Instagram" />
+            </a>
+        </div>
     </div>
 </footer>
 
 <style>
     footer {
-        align-items: flex-end;
-        background-color: var(--odd-purple-dark);
+        background-color: var(--odd-purple-medium);
+        border-top-left-radius: 0.625rem;
+        border-top-right-radius: 0.625rem;
         display: flex;
-        justify-content: space-between;
     }
 
     footer.odd-studio {
-        background-color: var(--odd-purple-dark);
+        background-color: var(--odd-purple-medium);
     }
 
     footer.odd-experiments {
@@ -47,7 +54,12 @@
         cursor: pointer;
     }
 
-    div {
+    #footer-social-media {
+        align-items: flex-end;
+        display: flex;
+    }
+
+    #footer-social-media div {
         align-items: center;
         display: flex;
         justify-content: center;
@@ -55,38 +67,211 @@
 
     @media only screen and (max-width: 600px) {
         footer {
-            padding: 1.5rem 7.5vw 2rem 7.5vw;
+            align-items: center;
+            border-bottom: 0;
+            flex-direction: column;
+            gap: 0;
+            justify-content: center;
+            padding: 0;
         }
 
-        footer > a,
-        footer > a img {
-            height: 3rem;
-            width: calc(3rem * 2.35);
+        #footer-social-media {
+            background-color: var(--odd-purple-dark);
+            gap: 0;
+            justify-content: space-between;
+            padding: 2rem 7.5vw;
+            width: 100%;
         }
 
-        footer > div a,
-        footer > div img {
+        #footer-social-media div {
+            gap: 1rem;
+        }
+
+        #footer-social-media a,
+        #footer-social-media a img {
             height: 1.5rem;
             width: 1.5rem;
         }
 
-        div {
-            gap: 1rem;
+        #footer-social-media > a,
+        #footer-social-media > a img {
+            height: 3rem;
+            width: calc(3rem * 2.35);
         }
     }
 
     @media only screen and (min-width: 600px) {
+        footer {
+            align-items: center;
+            border-bottom: 0;
+            flex-direction: column;
+            gap: 0;
+            justify-content: center;
+            padding: 0;
+        }
+
+        #footer-social-media {
+            background-color: var(--odd-purple-dark);
+            gap: 0;
+            justify-content: space-between;
+            padding: 2rem 12.5vw;
+            width: 100%;
+        }
+
+        #footer-social-media div {
+            gap: 1rem;
+        }
+
+        #footer-social-media a,
+        #footer-social-media a img {
+            height: 1.5rem;
+            width: 1.5rem;
+        }
+
+        #footer-social-media > a,
+        #footer-social-media > a img {
+            height: 3rem;
+            width: calc(3rem * 2.35);
+        }
     }
 
     @media only screen and (min-width: 768px) {
+        footer {
+            align-items: center;
+            border-bottom: 0;
+            flex-direction: column;
+            gap: 0;
+            justify-content: center;
+            padding: 0;
+        }
+
+        #footer-social-media {
+            background-color: var(--odd-purple-dark);
+            gap: 0;
+            justify-content: space-between;
+            padding: 2rem 15vw;
+            width: 100%;
+        }
+
+        #footer-social-media div {
+            gap: 1.5rem;
+        }
+
+        #footer-social-media a,
+        #footer-social-media a img {
+            height: 1.75rem;
+            width: 1.75rem;
+        }
+
+        #footer-social-media > a,
+        #footer-social-media > a img {
+            height: 3.5rem;
+            width: calc(3.5rem * 2.35);
+        }
     }
 
     @media only screen and (min-width: 1024px) {
+        footer {
+            align-items: center;
+            border-bottom: 0;
+            flex-direction: column;
+            gap: 0;
+            justify-content: center;
+            padding: 0;
+        }
+
+        #footer-social-media {
+            background-color: var(--odd-purple-dark);
+            gap: 0;
+            justify-content: space-between;
+            padding: 2rem 20vw;
+            width: 100%;
+        }
+
+        #footer-social-media div {
+            gap: 1.5rem;
+        }
+
+        #footer-social-media a,
+        #footer-social-media a img {
+            height: 2rem;
+            width: 2rem;
+        }
+
+        #footer-social-media > a,
+        #footer-social-media > a img {
+            height: 4rem;
+            width: calc(4rem * 2.35);
+        }
     }
 
     @media only screen and (min-width: 1280px) {
+        footer {
+            align-items: flex-end;
+            border-bottom: 3rem solid var(--odd-purple-dark);
+            flex-direction: row;
+            gap: 7.5rem;
+            justify-content: space-between;
+            padding: 3.75rem 5rem 2.5rem 5rem;
+        }
+
+        #footer-social-media {
+            background-color: transparent;
+            gap: 5rem;
+            justify-content: flex-end;
+            padding: 0;
+            width: 50%;
+        }
+
+        #footer-social-media div {
+            gap: 1.5rem;
+        }
+
+        #footer-social-media a,
+        #footer-social-media a img {
+            height: 2.25rem;
+            width: 2.25rem;
+        }
+
+        #footer-social-media > a,
+        #footer-social-media > a img {
+            height: 4.5rem;
+            width: calc(4.5rem * 2.35);
+        }
     }
 
     @media only screen and (min-width: 1536px) {
+        footer {
+            align-items: flex-end;
+            border-bottom: 3.5rem solid var(--odd-purple-dark);
+            flex-direction: row;
+            gap: 10rem;
+            justify-content: space-between;
+            padding: 5rem 7.5rem 3.75rem 7.5rem;
+        }
+
+        #footer-social-media {
+            background-color: transparent;
+            gap: 7.5rem;
+            justify-content: flex-end;
+            padding: 0;
+            width: 50%;
+        }
+
+        #footer-social-media div {
+            gap: 1.5rem;
+        }
+
+        #footer-social-media a,
+        #footer-social-media a img {
+            height: 2.25rem;
+            width: 2.25rem;
+        }
+
+        #footer-social-media > a,
+        #footer-social-media > a img {
+            height: 4.5rem;
+            width: calc(4.5rem * 2.35);
+        }
     }
 </style>
--- src/lib/components/Subscription.svelte
+++ src/lib/components/Subscription.svelte
@@ -31,7 +31,7 @@
             console.error(e);
         }
 
-        buttonText = 'confirmation e-mail sent ✨';
+        buttonText = 'subscribed!';
         buttonState = 'sent';
     };
 
@@ -45,13 +45,17 @@
         Join our community and receive updates on our projects, available
         courses, and job opportunities.
     </p>
-    <input type="text" bind:value={email} placeholder={'EMAIL HERE'} />
-    <button
-        class={buttonState}
-        on:click={buttonState === 'active' ? subscribe : () => {}}
-    >
-        <span>{buttonText}</span>
-    </button>
+    <div>
+        <input type="text" bind:value={email} placeholder={'EMAIL HERE'} />
+        <button
+            class={buttonState}
+            type="button"
+            on:click={buttonState === 'active' ? subscribe : () => {}}
+        >
+            <span>{buttonText}</span>
+            <span />
+        </button>
+    </div>
 </div>
 
 <style>
@@ -61,18 +65,21 @@
 
     div.odd-studio {
         background-color: var(--odd-purple-medium);
+        border-radius: inherit;
     }
 
     div.odd-experiments {
         background-color: var(--odd-turquoise-medium);
+        border-radius: inherit;
     }
 
     div.odd-education {
         background-color: var(--odd-blue-medium);
+        border-radius: inherit;
     }
 
     h3:nth-of-type(1) {
-        color: var(--odd-white);
+        color: var(--odd-gray-light);
         font-family: 'Newsreader', serif;
         font-style: italic;
         font-weight: 400;
@@ -100,34 +107,42 @@
     }
 
     p {
-        color: var(--odd-white);
+        color: var(--odd-gray-light);
         font-family: 'Switzer', sans-serif;
         letter-spacing: -0.04rem;
-        text-wrap: balance;
+        width: 90%;
+    }
+
+    div > div {
+        background-color: transparent;
+        display: flex;
+        justify-content: flex-end;
+        padding: 0;
     }
 
     input {
         appearance: none;
         background: transparent;
-        border-bottom: 1px solid var(--odd-gray-light);
         color: var(--odd-gray-light);
+        cursor: text;
         display: block;
         font-family: 'Switzer', sans-serif;
         font-weight: 400;
         letter-spacing: -0.04rem;
+        opacity: 0.75;
         outline: none;
         width: 100%;
     }
 
     input::placeholder {
         color: var(--odd-gray-light);
-        opacity: 0.5;
     }
 
     button {
         align-items: center;
         background-color: var(--odd-gray-light);
         border: 1px solid var(--odd-gray-dark);
+        border-radius: 3.75rem;
         color: var(--odd-gray-dark);
         cursor: pointer;
         display: flex;
@@ -138,57 +153,339 @@
         margin: 0 auto;
     }
 
-    button span {
+    button,
+    button * {
         cursor: pointer;
+    }
+
+    button.disabled,
+    button.disabled * {
+        opacity: 0.4;
+        cursor: not-allowed;
+    }
+
+    button.sending,
+    button.sending * {
+        cursor: wait;
+    }
+
+    button.sent,
+    button.sent * {
+        cursor: auto;
+    }
+
+    .odd-studio button.sent {
+        background-color: var(--odd-turquoise-light);
+    }
+
+    .odd-experiments button.sent {
+        background-color: var(--odd-yellow-medium);
+    }
+
+    .odd-education button.sent {
+        background-color: var(--odd-pink-light);
+    }
+
+    button.sending span:nth-child(2),
+    button.sent span:nth-child(2) {
+        display: none;
+    }
+
+    button span:nth-child(2) {
+        display: inline-block;
+        background-image: url('/icons/right-arrow-dark.svg');
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: contain;
     }
 
     @media only screen and (max-width: 600px) {
         div {
-            border-radius: 0.5rem 0.5rem 0 0;
-            padding: 2rem 7.5vw;
+            padding: 2.5rem 7.5vw 1.5rem 7.5vw;
         }
 
         h3:nth-of-type(1) {
             font-size: 2rem;
+            line-height: 75%;
         }
 
         h3:nth-of-type(2) {
             font-size: 2rem;
-            margin: -0.5rem 0 0.5rem 0;
+            margin-bottom: 0.5rem;
         }
 
         p {
             font-size: 1rem;
             margin-bottom: 1.25rem;
-            width: 90%;
+        }
+
+        div > div {
+            align-items: flex-start;
+            border-bottom: none;
+            flex-direction: column;
+            gap: 0;
+            padding: 0;
         }
 
         input {
+            border-bottom: 1px solid var(--odd-gray-light);
             font-size: 1rem;
             margin-bottom: 1.25rem;
             padding: 0.25rem;
         }
 
         button {
-            border-radius: 3.375rem;
             font-size: 1rem;
-            min-width: 8.5rem;
-            padding: 0.375rem 1rem;
+            gap: 0.5rem;
+            padding: 0.375rem 1.25rem;
+            width: 10rem;
+        }
+
+        button span:nth-child(2) {
+            height: 1rem;
+            width: 1rem;
         }
     }
 
     @media only screen and (min-width: 600px) {
+        div {
+            padding: 2.5rem 12.5vw 1.5rem 12.5vw;
+        }
+
+        h3:nth-of-type(1) {
+            font-size: 2rem;
+            line-height: 75%;
+        }
+
+        h3:nth-of-type(2) {
+            font-size: 2rem;
+            margin-bottom: 0.5rem;
+        }
+
+        p {
+            font-size: 1rem;
+            margin-bottom: 1.25rem;
+        }
+
+        div > div {
+            align-items: flex-start;
+            border-bottom: none;
+            flex-direction: column;
+            gap: 0;
+            padding: 0;
+        }
+
+        input {
+            border-bottom: 1px solid var(--odd-gray-light);
+            font-size: 1rem;
+            margin-bottom: 1.25rem;
+            padding: 0.25rem;
+        }
+
+        button {
+            font-size: 1rem;
+            gap: 0.5rem;
+            padding: 0.375rem 1.25rem;
+            width: 10rem;
+        }
+
+        button span:nth-child(2) {
+            height: 1rem;
+            width: 1rem;
+        }
     }
 
     @media only screen and (min-width: 768px) {
+        div {
+            padding: 2.5rem 15vw 1.5rem 15vw;
+        }
+
+        h3:nth-of-type(1) {
+            font-size: 2.25rem;
+            line-height: 60%;
+        }
+
+        h3:nth-of-type(2) {
+            font-size: 2.25rem;
+            margin-bottom: 0.5rem;
+        }
+
+        p {
+            font-size: 1.125rem;
+            margin-bottom: 1.25rem;
+        }
+
+        div > div {
+            align-items: flex-start;
+            border-bottom: none;
+            flex-direction: column;
+            gap: 0;
+            padding: 0;
+        }
+
+        input {
+            border-bottom: 1px solid var(--odd-gray-light);
+            font-size: 1.125rem;
+            margin-bottom: 1.25rem;
+            padding: 0.5rem;
+        }
+
+        button {
+            font-size: 1.125rem;
+            gap: 0.75rem;
+            padding: 0.375rem 1.25rem;
+            width: 11rem;
+        }
+
+        button span:nth-child(2) {
+            height: 1.125rem;
+            width: 1.125rem;
+        }
     }
 
     @media only screen and (min-width: 1024px) {
+        div {
+            padding: 2.5rem 20vw 1.5rem 20vw;
+        }
+
+        h3:nth-of-type(1) {
+            font-size: 2.25rem;
+            line-height: 60%;
+        }
+
+        h3:nth-of-type(2) {
+            font-size: 2.25rem;
+            margin-bottom: 0.5rem;
+        }
+
+        p {
+            font-size: 1.125rem;
+            margin-bottom: 1.25rem;
+        }
+
+        div > div {
+            align-items: flex-start;
+            border-bottom: none;
+            flex-direction: column;
+            gap: 0;
+            padding: 0;
+        }
+
+        input {
+            border-bottom: 1px solid var(--odd-gray-light);
+            font-size: 1.125rem;
+            margin-bottom: 1.25rem;
+            padding: 0.5rem;
+        }
+
+        button {
+            font-size: 1.125rem;
+            gap: 0.75rem;
+            padding: 0.375rem 1.25rem;
+            width: 11rem;
+        }
+
+        button span:nth-child(2) {
+            height: 1.125rem;
+            width: 1.125rem;
+        }
     }
 
     @media only screen and (min-width: 1280px) {
+        div {
+            padding: 0;
+        }
+
+        h3:nth-of-type(1) {
+            font-size: 2.75rem;
+            line-height: 50%;
+        }
+
+        h3:nth-of-type(2) {
+            font-size: 2.75rem;
+            margin-bottom: 0.5rem;
+        }
+
+        p {
+            font-size: 1.25rem;
+            margin-bottom: 1.5rem;
+        }
+
+        div > div {
+            align-items: flex-end;
+            border-bottom: 1px solid var(--odd-gray-light);
+            flex-direction: row;
+            gap: 1.5rem;
+            padding: 0 0 0.375rem 0;
+        }
+
+        input {
+            border-bottom: none;
+            font-size: 1.25rem;
+            margin-bottom: 0;
+            padding: 0;
+            width: 100%;
+        }
+
+        button {
+            font-size: 1.25rem;
+            gap: 0.75rem;
+            padding: 0.375rem 1.25rem;
+            width: 16rem;
+        }
+
+        button span:nth-child(2) {
+            height: 1.25rem;
+            width: 1.25rem;
+        }
     }
 
     @media only screen and (min-width: 1536px) {
+        div {
+            padding: 0;
+        }
+
+        h3:nth-of-type(1) {
+            font-size: 2.75rem;
+            line-height: 50%;
+        }
+
+        h3:nth-of-type(2) {
+            font-size: 2.75rem;
+            margin-bottom: 0.5rem;
+        }
+
+        p {
+            font-size: 1.25rem;
+            margin-bottom: 1.5rem;
+        }
+
+        div > div {
+            align-items: flex-end;
+            border-bottom: 1px solid var(--odd-gray-light);
+            flex-direction: row;
+            gap: 1.5rem;
+            padding: 0 0 0.375rem 0;
+        }
+
+        input {
+            border-bottom: none;
+            font-size: 1.25rem;
+            margin-bottom: 0;
+            padding: 0;
+            width: 100%;
+        }
+
+        button {
+            font-size: 1.25rem;
+            gap: 0.75rem;
+            padding: 0.375rem 1.25rem;
+            width: 16rem;
+        }
+
+        button span:nth-child(2) {
+            height: 1.25rem;
+            width: 1.25rem;
+        }
     }
 </style>
