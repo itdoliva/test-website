fbc3b3734a7f87dcef2e51180393bc89c51c1e9c
Matheus Alves <contato.mathsalvs@gmail.com>
2024-03-19 17:01:00 -0300

    Remove unused components and standardize CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-    import Subscription from '@components/Subscription.svelte';
     import Footer from '@components/Footer.svelte';
     import CoverSection from '@sections/home/CoverSection.svelte';
     import DescriptionSection from '@sections/home/DescriptionSection.svelte';
@@ -17,8 +16,24 @@
 <BranchesSection />
 <ClientsSection />
 <ValuesSection />
-<Subscription />
 <Footer pattern="studio" />
 
 <style>
+    @media only screen and (max-width: 600px) {
+    }
+
+    @media only screen and (min-width: 600px) {
+    }
+
+    @media only screen and (min-width: 768px) {
+    }
+
+    @media only screen and (min-width: 1024px) {
+    }
+
+    @media only screen and (min-width: 1280px) {
+    }
+
+    @media only screen and (min-width: 1536px) {
+    }
 </style>
