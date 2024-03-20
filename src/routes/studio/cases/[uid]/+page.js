0d034ca244e2fc24774a618dbe13d9620a64e2c9
Matheus Alves <contato.mathsalvs@gmail.com>
2024-03-20 14:13:21 -0300

    Fix Firebase deployment bug (8/N)

--- src/routes/studio/cases/[uid]/+page.js
+++ src/routes/studio/cases/[uid]/+page.js
@@ -2,9 +2,8 @@ import { get } from 'svelte/store';
 import { locale } from '@stores/language';
 import { createClient } from '@utils/prismic';
 
-export const prerender = 'auto';
+export const prerender = false;
 export const ssr = false;
-export const csr = true;
 
 export const load = async ({ params, fetch, cookies }) => {
     const client = createClient({ fetch, cookies });
